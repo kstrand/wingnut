d9c942e26666eb63d4d3815bc4c3b3c2532a56d5
Keith <keith.strand@gmail.com>
2013-03-09 22:23:10 -0800

    add list of .js files to be precompiled in production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+
+
 //= require foundation
 //= require_tree .
 
-$(document).foundation();
+// $(document).foundation();
 
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,9 @@ Wingnut::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.precompile += %w( jquery.easing.1.3.js jquery.liquid-slider.js jquery.liquid-slider.min.js jquery.touchSwipe.min.js jquery.stickyFooter.js )
+
+  
 
   # Generate digests for assets URLs
   config.assets.digest = true
